e073f04f3bd708eb019e22d1d67660ec83ac6fca
ielemson <ielemson@gmail.com>
2026-04-11 18:24:41 +0100

    updated slider

--- resources/views/partials/home_slider.blade.php
+++ resources/views/partials/home_slider.blade.php
@@ -1,65 +1,80 @@
 <section class="home-slider position-relative pt-25 pb-20">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-9">
-                        <div class="position-relative">
-                            <div class="hero-slider-1 style-3 dot-style-1 dot-style-1-position-1">
-                                <div class="single-hero-slider single-animation-wrap">
-                                    <div class="container">
-                                        <div class="slider-1-height-3 slider-animated-1">
-                                            <div class="hero-slider-content-2">
-                                                <h4 class="animated">Trade-In Offer</h4>
-                                                <h2 class="animated fw-900">Supper Value Deals</h2>
-                                                <h1 class="animated fw-900 text-brand">On All Products</h1>
-                                                <p class="animated">Save more with coupons & up to 70% off</p>
-                                                <a class="animated btn btn-brush btn-brush-3"
-                                                    href="shop-product-right.html"> Shop Now </a>
-                                            </div>
-                                            <div class="slider-img">
-                                                <img src="assets/imgs/slider/slider-4.png" alt="">
-                                            </div>
-                                        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="position-relative">
+                    <div class="hero-slider-1 style-3 dot-style-1 dot-style-1-position-1">
+
+                        {{-- Slide 1 --}}
+                        <div class="single-hero-slider single-animation-wrap">
+                            <div class="container">
+                                <div class="slider-1-height-3 slider-animated-1">
+                                    <div class="hero-slider-content-2">
+                                        <h4 class="animated">Bluesky Mart</h4>
+                                        <h2 class="animated fw-900">Best Deals</h2>
+                                        <h1 class="animated fw-900 text-brand">Shop Essentials</h1>
+                                        <p class="animated">Top quality. Great prices.</p>
+                                        <a class="animated btn btn-brush btn-brush-3"
+                                           href="{{ route('page.products.shop') }}">
+                                            Shop Now
+                                        </a>
                                     </div>
-                                </div>
-                                <div class="single-hero-slider single-animation-wrap">
-                                    <div class="container">
-                                        <div class="slider-1-height-3 slider-animated-1">
-                                            <div class="hero-slider-content-2">
-                                                <h4 class="animated">Tech Promotions</h4>
-                                                <h2 class="animated fw-900">Tech Trending</h2>
-                                                <h1 class="animated fw-900 text-brand">Great Collection</h1>
-                                                <p class="animated">Save more with coupons & up to 20% off</p>
-                                                <a class="animated btn btn-brush btn-brush-3"
-                                                    href="shop-product-right.html"> Shop Now </a>
-                                            </div>
-                                            <div class="slider-img">
-                                                <img src="assets/imgs/slider/slider-5.png" alt="">
-                                            </div>
-                                        </div>
+                                    <div class="slider-img">
+                                        <img src="{{ asset('assets/imgs/slider/slider-4.png') }}" alt="Bluesky Mart Deals">
                                     </div>
                                 </div>
                             </div>
-                            <div class="slider-arrow hero-slider-1-arrow style-3"></div>
                         </div>
+
+                        {{-- Slide 2 --}}
+                        <div class="single-hero-slider single-animation-wrap">
+                            <div class="container">
+                                <div class="slider-1-height-3 slider-animated-1">
+                                    <div class="hero-slider-content-2">
+                                        <h4 class="animated">New In</h4>
+                                        <h2 class="animated fw-900">Fresh Picks</h2>
+                                        <h1 class="animated fw-900 text-brand">Trending Now</h1>
+                                        <p class="animated">Hot items. Don’t miss out.</p>
+                                        <a class="animated btn btn-brush btn-brush-3"
+                                           href="{{ route('page.products.shop') }}">
+                                            Shop Now
+                                        </a>
+                                    </div>
+                                    <div class="slider-img">
+                                        <img src="{{ asset('assets/imgs/slider/slider-5.png') }}" alt="Trending Products">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="col-lg-3 d-md-none d-lg-block">
-                        <div class="banner-img banner-1 wow fadeIn  animated home-3">
-                            <img class="border-radius-10" src="assets/imgs/banner/banner-5.jpg" alt="">
-                            <div class="banner-text">
-                                <span>Accessories</span>
-                                <h4>Save 17% on <br>Autumn Hat</h4>
-                                <a href="shop-grid-right.html">Shop Now <i class="fi-rs-arrow-right"></i></a>
-                            </div>
-                        </div>
-                        <div class="banner-img banner-2 wow fadeIn  animated mb-0">
-                            <img class="border-radius-10" src="assets/imgs/banner/banner-7.jpg" alt="">
-                            <div class="banner-text">
-                                <span>Smart Offer</span>
-                                <h4>Save 20% on <br>Eardrop</h4>
-                                <a href="shop-grid-right.html">Shop Now <i class="fi-rs-arrow-right"></i></a>
-                            </div>
-                        </div>
+                    <div class="slider-arrow hero-slider-1-arrow style-3"></div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 d-md-none d-lg-block">
+                <div class="banner-img banner-1 wow fadeIn animated home-3">
+                    <img class="border-radius-10" src="{{ asset('assets/imgs/banner/banner-5.jpg') }}" alt="Bluesky Mart Accessories">
+                    <div class="banner-text">
+                        <span>Accessories</span>
+                        <h4>Upgrade Your Style</h4>
+                        <a href="{{ route('page.products.shop') }}">
+                            Shop Now <i class="fi-rs-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="banner-img banner-2 wow fadeIn animated mb-0">
+                    <img class="border-radius-10" src="{{ asset('assets/imgs/banner/banner-7.jpg') }}" alt="Bluesky Mart Smart Offer">
+                    <div class="banner-text">
+                        <span>Hot Deals</span>
+                        <h4>Save Big Today</h4>
+                        <a href="{{ route('page.products.shop') }}">
+                            Shop Now <i class="fi-rs-arrow-right"></i>
+                        </a>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
